351f4b45d31ac22cd94cf883e9e4218405877571
Maksim Bezdrobnij <maksim.bezdrobnij@wallester.com>
2025-05-17 11:27:47 +0300

    Remove unnesassary parts

--- ArchitectureKit/Package.swift
+++ ArchitectureKit/Package.swift
@@ -6,19 +6,10 @@ let package = Package(
     name: "ArchitectureKit",
     platforms: [.iOS(.v18)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ArchitectureKit",
-            targets: ["ArchitectureKit"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "ArchitectureKit"),
-        .testTarget(
-            name: "ArchitectureKitTests",
-            dependencies: ["ArchitectureKit"]
+            targets: ["ArchitectureKit"]
         ),
-    ]
+    ],
+    targets: [.target(name: "ArchitectureKit")]
 )
--- Features/StockDetails/Package.swift
+++ Features/StockDetails/Package.swift
@@ -1,24 +1,15 @@
 // swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "StockDetails",
+    platforms: [.iOS(.v18)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "StockDetails",
-            targets: ["StockDetails"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "StockDetails"),
-        .testTarget(
-            name: "StockDetailsTests",
-            dependencies: ["StockDetails"]
+            targets: ["StockDetails"]
         ),
-    ]
+    ],
+    targets: [.target(name: "StockDetails")]
 )
