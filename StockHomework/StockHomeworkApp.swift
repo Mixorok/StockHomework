052750e434f9689eaf3b585dd882f5556905b285
Maksim Bezdrobnij <maksim.bezdrobnij@wallester.com>
2025-05-17 10:56:21 +0300

    Initial commit

---
+++ StockHomework/StockHomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockHomeworkApp.swift
+//  StockHomework
+//
+//  Created by Maksim Bezdrobnoi on 17.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct StockHomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
